858eee147d3b2bf93a980f52b685ebcfde581803
DiegoVera <diego.vera@alumnos.ucn.cl>
2021-10-02 17:28:42 -0300

    register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -97,7 +97,6 @@
                         </div>
 
                         <div class="form-group row">
-
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
