14b16664fa1dd07521868b055636f2f33ab04536
DiegoVera <diego.vera@alumnos.ucn.cl>
2021-12-02 13:52:55 -0300

    Agregado: Envio de correo con solicitudes

--- resources/views/emails/resultadoSolicitud.blade.php
+++ resources/views/emails/resultadoSolicitud.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <h1>Resultado de su solicitud</h1>
@@ -12,5 +15,12 @@
     <p>Tipo de la solicitud: {{$Tipo}}</p>
     <p>Ha sido: {{$Resultado}} </p>
     <p>Comentarios: </p>
+
+    <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Well done!</h4>
+        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+        <hr>
+        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+    </div>
 </body>
 </html>
